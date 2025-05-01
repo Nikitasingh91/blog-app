ef2500066673e343cbc2bd96b9451a7868442dae
prabhat <prabhatsinghgkp123@gmail.com>
2025-05-01 21:55:34 +0530

    hello

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -61,6 +61,7 @@ export const AuthProvider = ({ children }) => {
                 profiles,
                 isAthenticated,
                 setProfiles,
+                setProfile,
                 setIsAthenticated,
             }}
         >
--- frontend/src/dashboard/MyProfile.jsx
+++ frontend/src/dashboard/MyProfile.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useAuth } from "../context/AuthProvider";
 
 function MyProfile() {
-    const { profiles } = useAuth();
+    const { profile } = useAuth();
+    console.log("my profile", profile);
+
 
     return (
         <div>
@@ -10,13 +12,13 @@ function MyProfile() {
                 <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg w-full">
                     <div className="relative">
                         <img
-                            src={profiles?.photo?.url}
+                            src={profile?.photo?.url}
                             alt="avatar"
                             className="w-full h-48 object-cover"
                         />
                         <div className="absolute inset-x-0 bottom-0 transform translate-y-1/2">
                             <img
-                                src={profiles?.photo?.url}
+                                src={profile?.photo?.url}
                                 alt="avatar"
                                 className="w-24 h-24 rounded-full mx-auto border-4 border-gray-700"
                             />
@@ -24,16 +26,16 @@ function MyProfile() {
                     </div>
                     <div className="px-6 py-8 mt-2">
                         <h2 className="text-center text-2xl font-semibold text-gray-800">
-                            {profiles?.name}
+                            {profile?.name}
                         </h2>
                         <p className="text-center text-gray-600 mt-2">
-                            {profiles?.email}
+                            {profile?.email}
                         </p>
                         <p className="text-center text-gray-600 mt-2">
-                            {profiles?.phone}
+                            {profile?.phone}
                         </p>
                         <p className="text-center text-gray-600 mt-2">
-                            {profiles?.role}
+                            {profile?.role}
                         </p>
                     </div>
                 </div>
